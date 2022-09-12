8647b7e5bfd96170b2ee6fdb16ee61894b8e30fc
OscarNturibi <danoskario@gmail.com>
2022-09-12 02:45:22 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-	int n;
-	for (n = '0'; n <= '9'; n++)
-{
-	putchar(n);
-	putchar('\n');
-}
+	int d;
+
+	for (d = '0' ; d <= '9' ; d++)
+		putchar(d);
+		putchar('\n');
 	return (0);
 }
