444f2eba6d539335ce95956655b4cecff902b184
OscarNturibi <danoskario@gmail.com>
2022-09-12 03:52:43 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - print if the number is positive, zero, or negative
+ *
+ *Description:using the main function
+ *Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c= '0'; c <= '9'; c++)
+{
+		putchar(c);
+}
+	for (c = 'a'; c <= 'f'; c++)
+{
+		putchar(c);
+}
+{
+		putchar('\n');
+}
+	return (0);
+}
