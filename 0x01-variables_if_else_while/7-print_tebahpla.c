f6cfc48923c41b3b5c2cd1db2c55713e59dbc109
OscarNturibi <danoskario@gmail.com>
2022-09-12 03:28:20 -0700

    print tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,41 +1,21 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
-
 /**
-
-* main - print if the number is postive, zero, or negative
-
- *
-
- * Description: using the main function
-
- * Return: 0
-
+*main - print if the number is positive, zero, or negative
+ *Description:using the main function
+ *Return: 0
  */
-
 int main(void)
-
 {
-
 	char c;
 
 	for (c = 'z'; c >= 'a'; c--)
-
 {
-
 		putchar(c);
-
 }
-
 {
-
 		putchar('\n');
-
 }
-
 return (0);
-
 }
