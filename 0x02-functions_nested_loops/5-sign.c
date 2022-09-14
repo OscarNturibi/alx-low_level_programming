82eb547c63d7c708326127ea80039b95dea1dead
OscarNturibi <danoskario@gmail.com>
2022-09-14 04:45:06 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,16 @@ int print_sign(int n)
 	{
 	_putchar('+');
 	return (1);
-	}
+}
+
 	else if (n < 0)
 	{
-	_putchar('_')
+	_putchar('-')
+
 	return (-1);
 	}
 	else
-	{
+	
 	_putchar('0');
 	return (0);
 	}
