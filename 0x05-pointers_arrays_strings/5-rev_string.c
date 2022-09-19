d4f629aa1fbca6e7dd3a34fb9fbaf24a95014f80
OscarNturibi <danoskario@gmail.com>
2022-09-19 10:57:49 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -24,3 +24,13 @@ void rev_string(char *s)
 *@s: string
 *Return: the length of the given string
 */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
