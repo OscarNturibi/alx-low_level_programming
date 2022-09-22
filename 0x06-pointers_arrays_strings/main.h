965097c36cd1bae11eccacf5b677731d2b09ec91
OscarNturibi <danoskario@gmail.com>
2022-09-22 12:43:54 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,8 +6,8 @@
  *int prototypes()
  *char prototypes()
  */
-void reverse_array(int *a, int n);
 void print_number(int n);
+void reverse_array(int *a, int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
