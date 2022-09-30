2963d909bc67935f88485c4776e3a39d78b83c3b
OscarNturibi <danoskario@gmail.com>
2022-09-30 09:32:29 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
-#define main_h
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 int _putchar(char c);
-int check_num(char *str);
 
 #endif
